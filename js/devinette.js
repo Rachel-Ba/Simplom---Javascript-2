c85402a300108cb4674c7d2679c725bbe714bb57
RachelBa <42960560+Rachel-Ba@users.noreply.github.com>
2018-10-29 15:03:33 +0100

    Update devinette.js
    Adaptation avec choix du minimum, du maximum et du nombres de chances

--- js/devinette.js
+++ js/devinette.js
@@ -1,11 +1,17 @@
 alert("Bienvenue dans ce jeu de devinette !");
 
+var min = Number(prompt("Entrez un nombre minimum"));
+var max = Number(prompt("Entrez un nombre maximum"));
+var chance = Number(prompt("Entrez le nombre de tentatives"));
 
-var solution = Math.floor(Math.random() * 10);
 
-var nbrJoueur = Number(prompt("Entrez un nombre entre 0 et 10:"));
+minimum = Math.ceil(min);
+maximum = Math.floor(max);
+var solution = Math.floor(Math.random() * (maximum - minimum +1)) + minimum;
 
-var chance=3;
+//--------------------------------------------------------------------------------------------------------------------------------------
+
+var nbrJoueur = Number(prompt("Entrez un nombre entre " + minimum + " et " + maximum));
 
 if (nbrJoueur===solution)
 	{
@@ -14,29 +20,29 @@ if (nbrJoueur===solution)
 
 else if(nbrJoueur!==solution) 
 	{
-		for(var i=1;i<6;i++)
+		for(var i=1 ; i<chance ; i++)
 			{			
 				if(nbrJoueur<solution)
 					{
-						alert(nbrJoueur+" est trop petit, essayez encore ! Il vous reste "+chance+" chance !");
+						alert(nbrJoueur+" est trop petit, essayez encore ! Il vous reste "+ chance +" chance !");
 						chance--;
-						nbrJoueur = Number(prompt("Entrez un nombre nouveau nombre entre 0 et 10:"));
+						nbrJoueur = Number(prompt("Entrez un nombre entre " + minimum + " et " + maximum));
 					}
 				else if(nbrJoueur>solution)
 					{
-						alert(nbrJoueur+" est trop grand, essayez encore ! Il vous reste "+chance+" chance !");
+						alert(nbrJoueur+" est trop grand, essayez encore ! Il vous reste "+ chance +" chance !");
 						chance--;
-						nbrJoueur = Number(prompt("Entrez un nombre nouveau nombre entre 0 et 10:"));
+						nbrJoueur = Number(prompt("Entrez un nombre entre " + minimum + " et " + maximum));
 					}
 				else if(nbrJoueur===solution)
 					{
-						alert("Bravo ! La solution était bien "+solution+" Vous avez trouvé en "+i+" essais !");
+						alert("Bravo ! La solution était bien "+ solution +" Vous avez trouvé en "+ i +" essais !");
 						break
 					}
 				
 			}
 		if(chance===0)
 			{
-				alert("Vous avez perdu ! La solution était "+solution);
+				alert("Vous avez perdu ! La solution était "+ solution);
 			}
 	}
